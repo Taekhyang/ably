9a4f1337b16245d02d06c6ff4120a0fd84d448c2
taekhyang <tkkim@bud-on.com>
2022-09-02 20:00:28 +0900

    Added dummy `sms_sender` function

---
+++ core/utils/sms_sender.py
@@ -0,0 +1,8 @@
+def send_sms(phone: str, content: str):
+    """
+    Send Authentication Code via Mobile Phone
+    :param phone: Mobile Phone Number
+    :param content: SMS Message Content
+    :return: bool
+    """
+    return True
