bf5a3586b278cf9100a0bbff48335bba5537dd43
taekhyang <tkkim@bud-on.com>
2022-09-02 14:16:15 +0900

    Added permission dependency for dependency injection

---
+++ core/fastapi/dependencies/__init__.py
@@ -0,0 +1,12 @@
+from .permission import (
+    PermissionDependency,
+    IsAuthenticated,
+    AllowAll
+)
+
+
+__all__ = [
+    "PermissionDependency",
+    "IsAuthenticated",
+    "AllowAll",
+]
